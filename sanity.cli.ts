ebfadf3b2a8bf6617c67b8f3d97490e560db5014
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 21:03:56 +0100

    added studiohost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'e3rbrik8',
     dataset: 'production'
   },
+  studioHost: 'refonte-mmi',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
