3a84286b1b923e47d634ddc717fb3ee4a2302a57
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 21:01:56 +0100

    import types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,7 @@
-import {postType} from './postType'
+import { actualiteType } from './actualiteType'
+import { temoignageType } from './temoignageType'
+import { alumniType } from './alumniType'
+import { profType } from './profType'
+import { projetType } from './projetType'
 
-export const schemaTypes = [postType]
+export const schemaTypes = [actualiteType, temoignageType, alumniType, profType, projetType]
